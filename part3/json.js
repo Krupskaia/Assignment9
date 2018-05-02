0448f474322014c63a433cab2b3b72e5e643be30
Krupskaia <35939280+Krupskaia@users.noreply.github.com>
2018-05-01 20:32:35 -0700

    Assignment9
    Assignment9

---
+++ part3/json.js
@@ -0,0 +1,27 @@
+{"members":[
+	{
+		"name":"Patricia",
+		"title":"Vice President of IT",
+		"bio":"With over 10 years of business and technology advisory service experience in the Tongas, Ecuador, Canada, Botswana and the US, Patricia is the most seasoned and diversed member of the Vecta Corp. team."
+	},
+	{
+		"name":"Eduardo",
+		"title":"Director of Development",
+		"bio":"Damon is the Director of Development for Vecta Corp. Damon creates learning materials for Vecta Corp. as well as consulting for customers to integrate vSolutions into their production pipeline and business processes."
+	},
+	{
+		"name":"John",
+		"title":"Director of Human Resources",
+		"bio":"Herbert joined Vecta Corp. in October 1999 as Vecta Corp's first Human Resources Director. As such, he has overall human resources responsibility for Vecta Corp's operations worldwide."
+	},
+	{
+		"name":"Luz",
+		"title":"Vice President of Sales and Marketing",
+		"bio":"Luz serves as the Vice President of Sales and Marketing for Vecta Corp. In this role Luz oversees Vecta Corp's marketing and corporate communications efforts worldwide."
+	},
+	{
+		"name":"Krupskaia",
+		"title":"Founder and CEO",
+		"bio":"While Krupskaia is the founder and CEO of Vecta Corp, she is primarily known for being the pioneer and world leader of creating vSolutions."
+	}
+]}
